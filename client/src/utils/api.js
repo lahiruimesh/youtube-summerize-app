c2a1c76251f814493ebef822bbdd60af865db9a2
lahiru imesh <imeshbl.22@uom.lk>
2025-08-23 01:43:30 +0530

    made some changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://youtube-summerize-app.up.railway.app/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://youtube-summerize-app.up.railway.app',
   withCredentials: true,
 });
 
